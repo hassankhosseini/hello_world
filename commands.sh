b3540ae529b77a929d2271e191c5e69e1d06c855
Ali <alikhajeh1@googlemail.com>
2016-12-06 18:41:36 +0000

    Add fake worker loops

--- commands.sh
+++ commands.sh
@@ -3,11 +3,21 @@
 if [ "$ENTRYPOINT" = "workers" ]
 then
   echo Starting workers
-  ruby -e "puts 'Fake workers running...'"
+  while :
+  do
+    ruby -e "puts 'Fake worker running...'"
+    sleep 1
+  done
+
 elif [ "$ENTRYPOINT" = "admin_ui" ]
 then
   echo Starting admin_ui
-  ruby -e "puts 'Fake admin_ui is running...'"
+  while :
+  do
+    ruby -e "puts 'Fake admin_ui is running...'"
+    sleep 1
+  done
+
 else
   echo Starting web process
   bundle exec puma -C puma.rb
