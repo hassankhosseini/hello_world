ed65d54c072b7906944c1a7560c27898c4180756
Ali <alikhajeh1@googlemail.com>
2017-02-18 12:49:50 +0330

    See live link

--- post_push.sh
+++ post_push.sh
@@ -2,19 +2,15 @@ bundle exec rspec spec
 
 if [[ $? == 0 ]]
 then
-  export STATUS="success"
-else
-  export STATUS="failure"
-fi
-
-curl "https://api.github.com/repos/abarcloud/hello_world/statuses/$OPENSHIFT_BUILD_COMMIT?access_token=$GH_TOKEN" \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d "{\"state\": \"$STATUS\", \"description\": \"AbarCloud\", \"target_url\": \"https://dashboard.abarcloud.com/console/project/$OPENSHIFT_BUILD_NAMESPACE/browse/builds/helloworld/$OPENSHIFT_BUILD_NAME?tab=logs\"}"
-
-if [[ $STATUS == "success" ]]
-then
+  curl "https://api.github.com/repos/abarcloud/hello_world/statuses/$OPENSHIFT_BUILD_COMMIT?access_token=$GH_TOKEN" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d "{\"state\": \"success\", \"description\": \"Live AbarCloud Link\", \"target_url\": \"https://helloworld-ci-cd.abar.cloud/\"}"
   exit 0
 else
+  curl "https://api.github.com/repos/abarcloud/hello_world/statuses/$OPENSHIFT_BUILD_COMMIT?access_token=$GH_TOKEN" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d "{\"state\": \"failure\", \"description\": \"AbarCloud\", \"target_url\": \"https://dashboard.abarcloud.com/console/project/$OPENSHIFT_BUILD_NAMESPACE/browse/builds/helloworld/$OPENSHIFT_BUILD_NAME?tab=logs\"}"
   exit 1
 fi
