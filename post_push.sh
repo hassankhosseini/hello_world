fe25fbe1cf495588e0db3045587c58bfff015b3a
Ali <alikhajeh1@googlemail.com>
2017-02-18 12:25:06 +0330

    Fix GH status URL

--- post_push.sh
+++ post_push.sh
@@ -7,7 +7,7 @@ else
   export STATUS="failure"
 fi
 
-curl "https://api.github.com/repos/alikhajeh1/hello_world/statuses/$OPENSHIFT_BUILD_COMMIT?access_token=$GH_TOKEN" \
+curl "https://api.github.com/repos/abarcloud/hello_world/statuses/$OPENSHIFT_BUILD_COMMIT?access_token=$GH_TOKEN" \
   -H "Content-Type: application/json" \
   -X POST \
   -d "{\"state\": \"$STATUS\", \"description\": \"AbarCloud\", \"target_url\": \"https://dashboard.abarcloud.com/console/project/$OPENSHIFT_BUILD_NAMESPACE/browse/builds/helloworld/$OPENSHIFT_BUILD_NAME?tab=logs\"}"
