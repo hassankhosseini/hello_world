e532e8dbed342cbcad8b3c637a2a1ad7050db413
Ali Khajeh-Hosseini <alikhajeh1@googlemail.com>
2017-06-13 19:03:23 +0430

    Create hello.rb

--- hello.rb
+++ hello.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 get '/' do
   puts 'Hello world requested...'
   puts "Sleeping at #{Time.now.utc}"
-  sleep(15)
+  sleep(7)
   puts "Woke up at #{Time.now.utc}"
   erb :index
 end
